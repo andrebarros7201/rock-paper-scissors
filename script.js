3eaacb26617012d5364d9fd90b41b4191d6e5bc3
André Barros <andrebarros7201@gmail.com>
2024-04-12 21:05:18 +0100

    Add Event Listeners and Full Game

--- script.js
+++ script.js
@@ -1,54 +1,63 @@
+const buttons = document.querySelectorAll('button');
+console.log(buttons);
+
+let playerScore = 0;
+let computerScore = 0;
+
+const player = document.querySelector('#playerScore');
+const computer = document.querySelector('#computerScore');
+
+player.textContent = `Player: ${playerScore}`;
+computer.textContent = `${playerScore} :Computer`;
+
 function getComputerChoice(){
     let number = Math.floor(Math.random() * (4 - 1) + 1);
     if (number == 1){
-        return 'Paper';
+        return 'paper';
     } else if (number == 2){
-        return 'Rock'
+        return 'rock'
     }else{
-        return "Scissors"
+        return "scissors"
     }
+}
+
+function checkWinner() {
+    if (playerScore === 5) {
+        alert('Player Wins!');
+        resetScore();
+    }else if (computerScore === 5){
+        alert('Computer Wins!');
+        resetScore();
+    }
+}
+
+function resetScore(){
+    playerScore = 0;
+    computerScore = 0;
+
+    player.textContent = `Player: ${playerScore}`;
+    computer.textContent = `${playerScore} :Computer`;
 }
 
 function playRound(playerChoice, computerChoice){
-    let playerChoiceLower = playerChoice.toLowerCase();
-    if ((playerChoiceLower == 'rock' && computerChoice == 'Rock') || (playerChoiceLower == 'paper' && computerChoice == 'Paper') || (playerChoiceLower == 'scissors' && computerChoice == 'Scissors')){
+    if ((playerChoice == 'rock' && computerChoice == 'rock') || (playerChoice == 'paper' && computerChoice == 'paper') || (playerChoice == 'scissors' && computerChoice == 'scissors')){
         return 'Draw!';
-    } else if((playerChoiceLower == 'rock' && computerChoice == 'Scissors') || (playerChoiceLower == 'paper' && computerChoice == 'Rock') || (playerChoiceLower == 'scissors' && computerChoice == 'Paper')){
-        return 'Player Wins!'
+    } else if((playerChoice == 'rock' && computerChoice == 'scissors') || (playerChoice == 'paper' && computerChoice == 'rock') || (playerChoice == 'scissors' && computerChoice == 'paper')){ 
+        playerScore++;
+        player.textContent = `Player: ${playerScore}`;
     }else{
-        return 'Computer Wins!'
+        computerScore++;
+        computer.textContent = `${computerScore} :Computer`;
     }
+    checkWinner();
 }
 
-function playGame(){
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    while (playerScore != 5 && computerScore != 5){
-        let computerChoice = getComputerChoice();
-        playerChoice = prompt('Choose Rock, Paper or Scissors');
-
-        console.log("Computer: " + computerChoice);
-        console.log("Player: " + playerChoice);
-        let result = playRound(playerChoice, computerChoice);
-        console.log(result);
-
-        if(result.charAt(0) == 'P'){
-            playerScore++;
-            console.log(playerScore);
-        }else if(result.charAt(0) == 'C'){
-            computerScore++;
-            console.log(computerScore);
-        }
-
-        if(playerScore == 5){
-            return 'Player Wins The Game!';
-        }else if(computerScore == 5){
-            return 'Computer Wins The Game!';
-        }
-
-    }
-}
-
-console.log(playGame());
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playerChoice = button.id;
+        computerChoice = getComputerChoice();
+        console.log(computerChoice);
+        playRound(playerChoice, computerChoice);
+    });
+});
 
